4f757032ea4d8cf60c6f1241cf00c54f192ddba4
Kevin Huang <mail2yizhou@gmail.com>
2019-06-16 20:22:47 +1000

    break in foreach loop

--- SoccerStats/Program.cs
+++ SoccerStats/Program.cs
@@ -48,6 +48,7 @@ namespace SoccerStats
                     {
                         result.SentimentScore = sentiment.Score;
                     }
+                    break;
 
                 }
             }
